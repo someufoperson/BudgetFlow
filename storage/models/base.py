e20307622c6aae5d8b7c6170cf28a55733515f75
Artem Tsyganov <artemtsy38@yandex.ru>
2026-07-14 20:30:54 +0800

    edit: add to Base class __repr__

--- storage/models/base.py
+++ storage/models/base.py
@@ -2,4 +2,10 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    pass  # TODO: add __repr__
+    def __repr__(self):
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
