9eb3ef855538db7fc13325d373fed00ba682bf37
Artem Tsyganov <artemtsy38@yandex.ru>
2026-07-15 15:23:55 +0800

    add: expense_transaction_repository.py with create, delete and select func

---
+++ storage/repositories/expense_transaction_repository.py
@@ -0,0 +1,76 @@
+from datetime import date, datetime, time, timedelta
+from decimal import Decimal
+
+from sqlalchemy import delete, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from storage.models.expense_transaction import ExpenseTransaction, ExpenseType
+
+
+class ExpenseTransactionRepository:
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def create(
+        self,
+        name: str,
+        expense_type: ExpenseType,
+        amount: Decimal,
+        currency_id: int,
+    ) -> ExpenseTransaction:
+        expense_transaction = ExpenseTransaction(
+            name=name, expense_type=expense_type, amount=amount, currency_id=currency_id
+        )
+        self._session.add(expense_transaction)
+        await self._session.flush()
+        return expense_transaction
+
+    async def delete(self, id: int) -> bool:
+        result = await self._session.execute(
+            delete(ExpenseTransaction)
+            .where(ExpenseTransaction.id == id)
+            .returning(ExpenseTransaction.id),
+        )
+        return result.scalar_one_or_none() is not None
+
+    async def select(
+        self,
+        *,
+        expense_type: ExpenseType | None = None,
+        currency_id: int | None = None,
+        date_from: date | None = None,
+        date_to: date | None = None,
+    ) -> list[ExpenseTransaction]:
+        if date_from is not None and date_to is not None:
+            if date_from > date_to:
+                raise ValueError("date_from cannot be later than date_to")
+
+        stmt = select(ExpenseTransaction)
+
+        if expense_type is not None:
+            stmt = stmt.where(
+                ExpenseTransaction.expense_type == expense_type,
+            )
+
+        if currency_id is not None:
+            stmt = stmt.where(
+                ExpenseTransaction.currency_id == currency_id,
+            )
+
+        if date_from is not None:
+            start_datetime = datetime.combine(date_from, time.min)
+            stmt = stmt.where(ExpenseTransaction.created_at >= start_datetime)
+
+        if date_to is not None:
+            end_datetime = datetime.combine(
+                date_to + timedelta(days=1),
+                time.min,
+            )
+            stmt = stmt.where(
+                ExpenseTransaction.created_at < end_datetime,
+            )
+
+        stmt = stmt.order_by(ExpenseTransaction.created_at.desc())
+
+        result = await self._session.scalars(stmt)
+        return list(result.all())
