68253f32677c0c0233e69bb0576b97ad19d54c32
Artem Tsyganov <artemtsy38@yandex.ru>
2026-07-15 21:28:19 +0800

    fix: delete excess import and change misstake in line 12 (after String(16) before String(length=16)

--- storage/models/currency.py
+++ storage/models/currency.py
@@ -1,5 +1,3 @@
-from enum import StrEnum
-
 from sqlalchemy import String, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -11,7 +9,7 @@ class Currency(AbstractModel):
     __tablename__ = "currencies"
 
     name: Mapped[str] = mapped_column(String(length=64), nullable=False)
-    code: Mapped[str] = mapped_column(String(16), nullable=False, unique=True)
+    code: Mapped[str] = mapped_column(String(length=16), nullable=False, unique=True)
     currency_type: Mapped[CurrencyType] = mapped_column(
         Enum(
             CurrencyType,
